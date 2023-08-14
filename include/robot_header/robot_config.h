074b7c204d1a915e62ea181f5475ac7e05af567b
Michael A <139656581+micade145@users.noreply.github.com>
2023-08-14 15:01:39 -0500

    first PROS commit 4148z

---
+++ include/robot_header/robot_config.h
@@ -0,0 +1,43 @@
+#ifndef ROBOT_CONFIG_H
+#define ROBOT_CONFIG_H
+#include "main.h"
+
+// Alliance color 
+extern bool isRed;
+
+// Constants
+extern double BACK_ENC_OFFSET;
+
+// Robot
+extern pros::Controller controller;
+
+// Drive Motors
+// extern pros::Motor leftDrive;
+// extern pros::Motor rightDrive;
+
+// extern pros::Motor leftFrontDrive;
+// extern pros::Motor leftBackDrive;
+// extern pros::Motor leftTopDrive;
+// extern pros::Motor rightFrontDrive;
+// extern pros::Motor rightBackDrive;
+// extern pros::Motor rightTopDrive;
+
+// Drive Motor Groups
+// extern pros::Motor_Group leftDrive;
+// extern pros::Motor_Group rightDrive;
+
+// Misc Motors
+// extern pros::Motor flywheel;
+// extern pros::Motor intake;
+
+// // Sensors
+// extern pros::Vision vision;
+// extern pros::Imu inertial;
+// extern pros::Rotation frontEnc;
+// extern pros::Rotation sideEnc;
+
+// // Pneumatics (Digital I/O)
+// extern pros::ADIDigitalOut angler;
+// extern pros::ADIDigitalOut indexer;
+// extern pros::ADIDigitalOut endgame;
+#endif
---
+++ src/robot/robot_config.cpp
@@ -0,0 +1,40 @@
+#include "robot_header/robot_config.h"
+
+// Alliance color 
+bool isRed = false;
+
+// Constants
+double BACK_ENC_OFFSET = 0.0;
+
+// Robot
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+// Drive Motors
+// pros::Motor leftDrive;
+// pros::Motor rightDrive;
+
+//  pros::Motor leftFrontDrive;
+//  pros::Motor leftBackDrive;
+//  pros::Motor leftTopDrive;
+//  pros::Motor rightFrontDrive;
+//  pros::Motor rightBackDrive;
+//  pros::Motor rightTopDrive;
+
+// Drive Motor Groups
+//  pros::Motor_Group leftDrive;
+//  pros::Motor_Group rightDrive;
+
+// Misc Motors
+//  pros::Motor flywheel;
+//  pros::Motor intake;
+
+// // Sensors
+//  pros::Vision vision;
+//  pros::Imu inertial;
+//  pros::Rotation frontEnc;
+//  pros::Rotation sideEnc;
+
+// // Pneumatics (Digital I/O)
+//  pros::ADIDigitalOut angler;
+//  pros::ADIDigitalOut indexer;
+//  pros::ADIDigitalOut endgame;
