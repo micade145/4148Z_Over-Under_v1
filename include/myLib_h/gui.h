7fd93317bb1f306f7028fce6757a1316b6fd275e
Michael A <139656581+micade145@users.noreply.github.com>
2023-10-07 22:53:50 -0500

    gui resources added

--- include/myLib_h/gui.h
+++ include/myLib_h/gui.h
@@ -2,6 +2,20 @@
 #define GUI_H
 #include "main.h"
 
+
+// Nice Tutorial
+// https://team81k.github.io/ProsLVGLTutorial/
+
+// Example
+// https://github.com/kunwarsahni01/Vex-Autonomous-Selector
+
+// Discussion 
+// https://www.vexforum.com/t/questions-about-lvgl-examples-of-v5-displays/58778
+
+// Official documentation / GitHub
+// https://docs.lvgl.io/8.3/get-started/index.html
+// https://github.com/lvgl/lvgl
+
 extern void initDisplayGUI();
 
 #endif
